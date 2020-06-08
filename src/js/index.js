f243baa1e419014f22b56d12f5b471001a50e0c3
Mariusz Wójcik <36dragan@gmail.com>
2020-06-08 20:55:02 +0200

    add js file

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,36 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const name = 'Mariusz';
+const age = 18;
+const hook = 22;
+
+console.log(`Hej, nazywam się ${name} i mam ${age} lat z hakiem.`);
+console.log(`Hak ma ledwie ${hook} lata ;)`);
+
+
+const js_code = document.querySelector('.article-section__title--js');
+console.log(js_code);
+
+if (js_code != null)
+js_code.innerHTML = 'here you are... <div class= "JS_box"> JS is here </div>';
+
+function welcome(age, name, hook) {
+    console.log(`Hej, nazywam się ${name} i mam ${age} lat z hakiem.`);
+    console.log(`Hak ma ledwie ${hook} lata ;)`);
+};
+
+welcome(age, name, hook);
+
+const welcome_new = (age, name, hook)=>{
+    console.log(`Hej, nazywam się ${name} i mam ${age} lat z hakiem.`);
+    console.log(`Hak ma ledwie ${hook} lata ;)`);  
+};
+
+welcome_new(age, name, hook);
+
+const hamburgher = document.querySelector('.hamburger--js');
+hamburgher.addEventListener('click', ()=>{
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
