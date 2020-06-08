b6849a3ed007a591ac154db7ff19c476b270a0ab
Mariusz Wójcik <36dragan@gmail.com>
2020-06-08 20:24:13 +0200

    update nauka-css.html

--- webpack.common.js
+++ webpack.common.js
@@ -63,5 +63,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/nauka-css.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "nauka-css.html",
+    }),
   ],
 };
